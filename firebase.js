8294d7d465c119e389e938b980f0632bbecf3c15
SaeedASC22 <sbashiru@stu.allstarcode.org>
2022-08-15 16:20:07 -0400

    Initial commit

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAU62-mKqqkXYJY0PVAs8GEpjD5wIZoKnQ",
+  authDomain: "firemessages-28ae4.firebaseapp.com",
+  databaseURL: "https://firemessages-28ae4-default-rtdb.firebaseio.com",
+  projectId: "firemessages-28ae4",
+  storageBucket: "firemessages-28ae4.appspot.com",
+  messagingSenderId: "650297507446",
+  appId: "1:650297507446:web:969f081e997fbb2357a957",
+  measurementId: "G-Y7HFNB046M"
+};
+
+// initialize the firebase app with this built-in function
+firebase.initializeApp(firebaseConfig);
